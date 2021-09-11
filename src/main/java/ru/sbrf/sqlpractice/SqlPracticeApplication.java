8fa087da62f3d4dd32d827ec5f2d426e7aca9f1a
no8ter <rudovskii5@mail.ru>
2021-09-11 10:38:53 +0300

    Initial commit

---
+++ src/main/java/ru/sbrf/sqlpractice/SqlPracticeApplication.java
@@ -0,0 +1,13 @@
+package ru.sbrf.sqlpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sbrf/sqlpractice/SqlPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sbrf.sqlpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
